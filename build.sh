489a45cd6ae69bebb5c21b78c8d2799c83f07709
Ivan Svetunkov <ivan@svetunkov.ru>
2022-10-07 18:41:18 +0100

    Proper structure.

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -ev
+
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+#
+# sleep 3
+#
+# git commit -am "New version of SBA compiled"
+#
+# git push
+#
+# git add * || true
+#
+# git commit -am "New version of SBA compiled" || true
+#
+# git push
