011ecec5a5a35c544997d3346ec93af476fba769
Ivan Svetunkov <ivan@svetunkov.ru>
2022-11-03 11:48:24 +0000

    New version of SBA compiled

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ set -ev
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
-#
-# sleep 3
-#
-# git commit -am "New version of SBA compiled"
-#
-# git push
+
+sleep 3
+
+git commit -am "New version of SBA compiled"
+
+git push
 #
 # git add * || true
 #
